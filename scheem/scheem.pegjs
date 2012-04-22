6423af8e51a7c2c95446f634bf26926af2f257f7
Alexandre Heaume <aheaume@gmail.com>
2012-04-22 21:38:50 +0200

    Add a PEG and some tests for Scheem.

---
+++ scheem/scheem.pegjs
@@ -0,0 +1,14 @@
+start        =  expr
+
+expr         =  _* e:scheem_expr _*                { return e; }
+
+scheem_expr  =  "'" e:expr                         { return ["quote", e]; }
+             /  x:symbol                           { return x; }
+             /  xs:list                            { return xs; }
+
+symbol       =  chars:[0-9a-zA-Z_?!+-=@#$%^&*/.]+  { return chars.join(""); }
+
+list         =  "(" xs:expr* ")"                   { return xs; }
+
+_            =  [ \t\r\n]+
+             /  ";;" [^\r\n]*
---
+++ scheem/test.js
@@ -0,0 +1,28 @@
+var peg = require('pegjs');
+var should = require('should');
+var fs = require('fs');
+
+fs.readFile('scheem.pegjs', 'ascii', function(err, data) {
+    if(err !== null) {
+      console.log(err);
+    } else {
+      var parse = peg.buildParser(data).parse;
+
+      var verify = function(code, expected) {
+        console.log("Verifying: " + code);
+        should.deepEqual(parse(code), expected);
+      }
+
+      verify("42", "42");
+      verify("quux", "quux");
+      verify("()", []);
+      verify("((()))", [[[]]]);
+      verify("(quux)", ["quux"]);
+      verify("(a b c)", ["a", "b", "c"]);
+      verify("(a (b c))", ["a", ["b", "c"]]);
+      verify("'quux", ["quote", "quux"]);
+      verify("'(quux)", ["quote", ["quux"]]);
+      verify("(quux) ;; comment", ["quux"]);
+      verify("(a\n;; comment\nb c)", ["a", "b", "c"]);
+    }
+});
