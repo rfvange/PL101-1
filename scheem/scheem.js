48d6415f94179a066423fc5f8ab7740e0f77bbc3
Alexandre Heaume <aheaume@gmail.com>
2012-05-14 15:12:47 +0200

    evalScheem can now look for variables in its environment

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -3,15 +3,25 @@ var evalScheem = function (expr, env) {
     if (typeof expr === 'number') {
         return expr;
     }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        return env[expr];
+    }
     // Look at head of list for operation
     switch (expr[0]) {
         case '+':
-            return evalScheem(expr[1]) + evalScheem(expr[2]);
+            return evalScheem(expr[1], env) +
+                   evalScheem(expr[2], env);
         case '-':
-            return evalScheem(expr[1]) - evalScheem(expr[2]);
+            return evalScheem(expr[1], env) -
+                   evalScheem(expr[2], env);
+            
         case '*':
-            return evalScheem(expr[1]) * evalScheem(expr[2]);
+            return evalScheem(expr[1], env) *
+                   evalScheem(expr[2], env);
         case '/':
-            return evalScheem(expr[1]) / evalScheem(expr[2]);
+            return evalScheem(expr[1], env) /
+                   evalScheem(expr[2], env);
     }
 };
+
