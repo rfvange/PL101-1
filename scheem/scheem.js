fc29be4c2ab799a269efc2e27110d2ca432b7b41
Alexandre Heaume <aheaume@gmail.com>
2012-05-14 15:52:22 +0200

    evalScheem can now compare expressions

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -18,6 +18,21 @@ var evalScheem = function (expr, env) {
                 result = evalScheem(expr[i], env);
             }
             return result;
+        case '=':
+            var eq = (evalScheem(expr[1], env) ===
+                      evalScheem(expr[2], env));
+            if (eq) return '#t';
+            return '#f';
+        case '<':
+            var lt = (evalScheem(expr[1], env) <
+                      evalScheem(expr[2], env));
+            if (lt) return '#t';
+            return '#f';
+        case '>':
+            var gt = (evalScheem(expr[1], env) >
+                      evalScheem(expr[2], env));
+            if (gt) return '#t';
+            return '#f';
         case 'define':
         case 'set!':
             env[expr[1]] = evalScheem(expr[2], env);
